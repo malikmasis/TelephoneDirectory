bc0db45b14f677509a6bd7de94298137d4b56a1a
Malik Masis <malik.masis@gmail.com>
2021-06-06 15:41:38 +0300

    init migration automatically in guide ms

--- src/Microservices/Guide/TelephoneDirectory.Guide/Program.cs
+++ src/Microservices/Guide/TelephoneDirectory.Guide/Program.cs
@@ -1,7 +1,10 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using Serilog.Events;
+using TelephoneDirectory.Guide.Data;
 
 namespace TelephoneDirectory.Guide
 {
@@ -9,7 +12,13 @@ namespace TelephoneDirectory.Guide
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<GuideDbContext>();
+                db.Database.Migrate();
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
