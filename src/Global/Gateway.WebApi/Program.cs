1ef9ea3878f9a17380c40ab9cc82a8a397f91e0c
Malik Masis <malik.masis@gmail.com>
2021-06-10 15:14:27 +0300

    removed https from getway

--- src/Global/Gateway.WebApi/Program.cs
+++ src/Global/Gateway.WebApi/Program.cs
@@ -16,7 +16,7 @@ namespace Gateway.WebApi
             Host.CreateDefaultBuilder(args)
             .ConfigureWebHostDefaults(webBuilder =>
             {
-                webBuilder.UseUrls(new string[] { "https://localhost:44382/" });
+                //webBuilder.UseUrls(new string[] { "https://localhost:44382/" });
                 webBuilder.UseStartup<Startup>();
             })
             .ConfigureAppConfiguration((hostingContext, config) =>
