66e0807937911b484116ad2f983ad8445e4c96f4
malik masis <malik.masis@gmail.com>
2022-07-16 14:28:28 +0300

    Configured ocelot for multiple actions of the golang example

--- src/Microservices/Golang/Example/main.go
+++ src/Microservices/Golang/Example/main.go
@@ -6,11 +6,14 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Merhaba %s", r.URL.Path[1:])
+	fmt.Fprintf(w, "Hello %s", r.URL.Path[1:])
 }
 
 func main() {
-	http.HandleFunc("/api/examplego", handler)
+
+	http.HandleFunc("/api/examplego/1", handler)
+	http.HandleFunc("/api/examplego/2", handler)
+
 	http.ListenAndServe(":9000", nil)
 
 	fmt.Println("Web Server")
