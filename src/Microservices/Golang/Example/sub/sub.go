d72bf65d5cd6e14a6ea3e10aa7bb83bc31d2736a
malik masis <malik.masis@gmail.com>
2023-01-08 13:36:35 +0300

    Added sending mail once consuming the published event

--- src/Microservices/Golang/Example/sub/sub.go
+++ src/Microservices/Golang/Example/sub/sub.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
+	"net/smtp"
 
 	"github.com/dapr/go-sdk/service/common"
 	daprd "github.com/dapr/go-sdk/service/http"
@@ -34,8 +36,38 @@ func main() {
 
 func eventHandler(ctx context.Context, e *common.TopicEvent) (retry bool, err error) {
 	log.Printf("event - PubsubName: %s, Topic: %s, ID: %s, Data: %s", e.PubsubName, e.Topic, e.ID, e.Data)
+
+	log.Printf("sending mail")
+
+	from := "malik.masis@gmail.com"
+	password := "psw"
+
+	// Receiver email address.
+	to := []string{
+		"malik.masis@hotmail.com",
+	}
+
+	// smtp server configuration.
+	smtpHost := "smtp.gmail.com"
+	smtpPort := "587"
+
+	// Message.
+	message := []byte("This is a test email message.")
+
+	// Authentication.
+	auth := smtp.PlainAuth("", from, password, smtpHost)
+
+	// Sending email.
+	errMail := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, message)
+	if errMail != nil {
+		fmt.Println(errMail)
+		return
+	}
+	fmt.Println("Email Sent Successfully!")
+	log.Printf("Email Sent Successfully!")
+
 	return false, nil
 }
 
-//dapr run --app-id sub --app-protocol http --app-port 8071 --dapr-http-port 3500 --log-level debug --components-path ./config go run sub/sub.go
-//dapr run --app-id sub --app-protocol http --app-port 8071 --dapr-http-port 3500 --log-level debug go run sub/sub.go
+//dapr run --app-id sub --app-protocol http --app-port 8023 --dapr-http-port 3500 --log-level debug --components-path ./config go run sub/sub.go
+//dapr run --app-id sub --app-protocol http --app-port 8023 --dapr-http-port 3500 --log-level debug go run sub/sub.go
