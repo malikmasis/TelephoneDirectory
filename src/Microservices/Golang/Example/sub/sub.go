09fe47892631ce348c2b8143836354b5ebbf1764
malik masis <malik.masis@gmail.com>
2023-01-09 00:58:15 +0300

    Implemented twillio for golang

--- src/Microservices/Golang/Example/sub/sub.go
+++ src/Microservices/Golang/Example/sub/sub.go
@@ -6,9 +6,12 @@ import (
 	"log"
 	"net/http"
 	"net/smtp"
+	"os"
 
 	"github.com/dapr/go-sdk/service/common"
 	daprd "github.com/dapr/go-sdk/service/http"
+	twilio "github.com/twilio/twilio-go"
+	openapi "github.com/twilio/twilio-go/rest/api/v2010"
 )
 
 // Subscription to tell the dapr what topic to subscribe.
@@ -37,35 +40,55 @@ func main() {
 func eventHandler(ctx context.Context, e *common.TopicEvent) (retry bool, err error) {
 	log.Printf("event - PubsubName: %s, Topic: %s, ID: %s, Data: %s", e.PubsubName, e.Topic, e.ID, e.Data)
 
-	log.Printf("sending mail")
+	//TODO - just for example -> will change it
+	if e.Topic == "neworder" {
+		log.Printf("sending mail")
 
-	from := "malik.masis@gmail.com"
-	password := "psw"
+		from := "malik.masis@gmail.com"
+		password := "psw"
 
-	// Receiver email address.
-	to := []string{
-		"malik.masis@hotmail.com",
+		// Receiver email address.
+		to := []string{
+			"malik.masis@hotmail.com",
+		}
+
+		// smtp server configuration.
+		smtpHost := "smtp.gmail.com"
+		smtpPort := "587"
+
+		// Message.
+		message := []byte("This is a test email message.")
+
+		// Authentication.
+		auth := smtp.PlainAuth("", from, password, smtpHost)
+
+		// Sending email.
+		errMail := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, message)
+		if errMail != nil {
+			fmt.Println(errMail)
+			return
+		}
+		fmt.Println("Email Sent Successfully!")
+		log.Printf("Email Sent Successfully!")
+	} else {
+		//export TWILIO_ACCOUNT_SID=xxxxxxxxx or setx TWILIO_ACCOUNT_SID xxxxxxxxx (windows) - need to restart pc
+		log.Printf("Sending sms: " + os.Getenv("TWILIO_ACCOUNT_SID") + " " + os.Getenv("TWILIO_AUTH_TOKEN"))
+
+		client := twilio.NewRestClient()
+
+		params := &openapi.CreateMessageParams{}
+
+		params.SetTo(os.Getenv("TWILIO_PHONE_NUMBER"))
+		params.SetFrom(os.Getenv("TWILIO_PHONE_NUMBER"))
+		params.SetBody("Hello from Golang!")
+
+		_, errPhone := client.Api.CreateMessage(params)
+		if errPhone != nil {
+			fmt.Println(errPhone.Error())
+		} else {
+			fmt.Println("SMS sent successfully!")
+		}
 	}
-
-	// smtp server configuration.
-	smtpHost := "smtp.gmail.com"
-	smtpPort := "587"
-
-	// Message.
-	message := []byte("This is a test email message.")
-
-	// Authentication.
-	auth := smtp.PlainAuth("", from, password, smtpHost)
-
-	// Sending email.
-	errMail := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, message)
-	if errMail != nil {
-		fmt.Println(errMail)
-		return
-	}
-	fmt.Println("Email Sent Successfully!")
-	log.Printf("Email Sent Successfully!")
-
 	return false, nil
 }
 
