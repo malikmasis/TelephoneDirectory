d85387a0ef5f884085b0c3e5282e60d08253a6e2
malik masis <malik.masis@gmail.com>
2023-01-13 12:28:15 +0300

    Handled the consumer side

---
+++ src/Microservices/Csharp/Report/TelephoneDirectory.Report.UnitTest/XUnitOutput.cs
@@ -0,0 +1,20 @@
+﻿using PactNet.Infrastructure.Outputters;
+using Xunit.Abstractions;
+
+namespace TelephoneDirectory.Report.UnitTest
+{
+    public class XUnitOutput : IOutput
+    {
+        private readonly ITestOutputHelper _output;
+
+        public XUnitOutput(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        public void WriteLine(string line)
+        {
+            _output.WriteLine(line);
+        }
+    }
+}
---
+++ src/Microservices/Csharp/Report/TelephoneDirectory.Report/Consumers/CustomerApiClient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TelephoneDirectory.Report.Consumers;
+
+public static class ConsumerApiClient
+{
+    static public async Task<HttpResponseMessage> ValidateDateTimeUsingProviderApi(string dateTimeToValidate, Uri baseUri)
+    {
+        using (var client = new HttpClient { BaseAddress = baseUri })
+        {
+            try
+            {
+                var response = await client.GetAsync($"/api/provider?validDateTime={dateTimeToValidate}");
+                return response;
+            }
+            catch (System.Exception ex)
+            {
+                throw new Exception("There was a problem connecting to Provider API.", ex);
+            }
+        }
+    }
+}
